452940d65b4c8afc6b2e3f5e931265ba943f25e8
Eric Eldredge <lettertwo@gmail.com>
2011-09-28 17:48:43 -0400

    Some sketchwork on a revised galleries api
    Gallery's nested Membership abstract can be extended to define custom fields.
    Gallery's nested GalleryMeta is required, and must define member_models list.
    An abstract Image model provides a base for writing member models.

--- galleries/models.py
+++ galleries/models.py
@@ -6,86 +6,98 @@ from django.db.models import Q
 import sys, imp, importlib
 
 
+class GalleryMetaNotDefined(Exception):
+    "The gallery is missing the required meta class."
+    pass
+
+
+class MemberModelsNotDefined(Exception):
+    "The gallery options do not define a list of allowed member models."
+    pass
+
+
+class MembershipClassNotDefined(Exception):
+    "The gallery does not define a membership class."
+    pass
+
+
 class ImageModel(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255, blank=True)
     original_image = models.ImageField(upload_to='galleries')
+
+    class __metaclass__(models.base.ModelBase):
+        def __init__(cls, class_name, bases, attrs):
+            # attrs['proxy'] = True
+            return models.base.ModelBase.__init__(cls, class_name, bases, attrs)
 
     def __unicode__(self):
         return self.title
 
     class Meta:
         ordering = ['title']
-    
-    class IKOptions:
-        default_image_field = 'original_image'
-
-
-# def create_membership_model(app_label, class_name, gallery_model, member_models, verbose_name=None):
-#     _app_label = app_label
-#     _verbose_name = verbose_name
-#     member_choices = Q()
-#     for member_model in member_models:
-#         member_choices |= Q(app_label=member_model._meta.app_label,
-#             model=member_model._meta.module_name)
-#     
-#     # TODO: Pull this out so that people can make their own custom membership classes without having to rewire everything.
-#     class DynamicGalleryMembership(models.Model):
-#         gallery = models.ForeignKey(gallery_model, related_name='memberships')
-#         content_type = models.ForeignKey(ContentType,
-#                 limit_choices_to=member_choices)
-#         object_id = models.PositiveIntegerField()
-#         item = generic.GenericForeignKey('content_type', 'object_id')
-#         sort_order = models.IntegerField(default=0)
-# 
-#         def __unicode__(self):
-#             return str(self.item)
-# 
-#         class __metaclass__(ModelBase):
-#             def __new__(cls, old_class_name, bases, attrs):
-#                 return ModelBase.__new__(cls, class_name, bases, attrs)
-# 
-#         class Meta:
-#             app_label = _app_label
-#             verbose_name = _verbose_name
-#             ordering = ['sort_order']
-# 
-#     return DynamicGalleryMembership
+        abstract = True
 
 
 class GalleryBase(ModelBase):
 
     def __init__(cls, class_name, bases, attrs):
         if [b for b in bases if isinstance(b, GalleryBase)]:
-            gallery_meta = getattr(cls, 'GalleryMeta')
+            try:
+                membership_class = getattr(cls, 'Membership')
+            except AttributeError:
+                raise MembershipClassNotDefined('%s must define a Membership class.' % class_name)
+            try:
+                gallery_meta = getattr(cls, 'GalleryMeta')
+            except AttributeError:
+                raise GalleryMetaNotDefined('%s must define GalleryMeta.' % class_name)
+            try:
+                member_models = getattr(gallery_meta, 'member_models')
+            except AttributeError:
+                raise MemberModelsNotDefined('%s.GalleryMeta must define a list of member_models.' % class_name)
+
             gallery_meta.gallery_class = cls
             cls._gallery_meta = gallery_meta
 
             # Don't inherit Membership classes.
-            membership_class = attrs.get('Membership')
-            print cls
-            print membership_class
-            if membership_class is not None:
+            try:
                 del attrs['Membership']
-            ModelBase.__init__(cls, class_name, bases, attrs)
+            except KeyError:
+                pass
             try:
                 delattr(cls, 'Membership')
             except AttributeError:
                 pass
 
-            # If there is no inner Membership class, create one.
-            if membership_class is None:
-                membership_class = type('Membership', (Gallery.Membership,), {
-                        '__module__': cls.__module__})
+            membership_class_name = '%sMembership' % class_name
+            membership_verbose_name = '%s Membership' % cls._meta.verbose_name
+
+            if (membership_class is Gallery.Membership):
+                _app_label = cls._meta.app_label
+                module_name = '%s.models' % _app_label
+                module = importlib.import_module(module_name)
+
+                class DynamicMembership(Gallery.Membership):
+                    class __metaclass__(Gallery.Membership.__metaclass__):
+                        def __new__(cls, name, bases, attrs):
+                            attrs['__module__'] = module_name
+                            return Gallery.Membership.__metaclass__.__new__(cls,
+                                membership_class_name, bases, attrs)
+                    class Meta:
+                        app_label = _app_label
+                        verbose_name = membership_verbose_name
+
+                membership_class = DynamicMembership
 
             # Augment the membership class with the relational fields. You can
             # "opt out" of this by not extending Gallery.Membership
+            # TODO: Make this less magical.
             if issubclass(membership_class, Gallery.Membership):
                 _update_membership(membership_class, cls)
 
-            cls.Membership = membership_class
-        else:
-            ModelBase.__init__(cls, class_name, bases, attrs)
+            setattr(cls, 'Membership', DynamicMembership)
+
+        ModelBase.__init__(cls, class_name, bases, attrs)
 
 
 def _update_membership(membership, gallery):
@@ -99,8 +111,6 @@ def _update_membership(membership, gallery):
                 related_name='memberships'),
         'content_type': models.ForeignKey(ContentType,
                 limit_choices_to=member_choices),
-        'object_id': models.PositiveIntegerField(),
-        'item': generic.GenericForeignKey('content_type', 'object_id'),
     }
     for k, v in fields.items():
         v.contribute_to_class(membership, k)
@@ -115,10 +125,19 @@ class Gallery(models.Model):
         return self.title
 
     class Membership(models.Model):
+        object_id = models.PositiveIntegerField()
+        item = generic.GenericForeignKey('content_type', 'object_id')
         sort_order = models.IntegerField(default=0)
 
         def __unicode__(self):
             return str(self.item)
 
+        class Meta:
+            ordering = ['sort_order']
+            abstract = True
+
     class GalleryMeta:
         pass
+
+    class Meta:
+        abstract = True
