6b05839ebeeb3cc32bec9ee78b2f2af3e3d4b1fe
Matthew Tretter <m@tthewwithanm.com>
2013-07-08 14:24:49 -0400

    Formatting cleanup

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 import os
 from distutils.core import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 README = read('README.markdown')
 
+
 setup(
-    name = "django-galleries",
-    version = "0.1",
+    name='django-galleries',
+    version='0.1',
     description='Simple Djagno galleries.',
     long_description=README,
 
-    author = 'Matthew Tretter',
-    author_email = 'matthew@exanimo.com',
-    packages = [
+    author='Matthew Tretter',
+    author_email='matthew@exanimo.com',
+    packages=[
         'galleries',
         'exampleapp',
     ],
-    install_requires = [
-        'django-imagekit>=0.3.6', # TODO: Relax this requirement a bit. I'm sure it works with a much earlier version.
+    install_requires=[
+        'django-imagekit>=0.3.6',
     ],
-    classifiers = [
+    classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
