fd0ba02f522249c36213fdf8cffcb134c2744418
Matthew Tretter <m@tthewwithanm.com>
2013-07-15 18:51:47 -0400

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = read('README.markdown')
 
 setup(
     name='django-galleries',
-    version='1.0.0',
+    version='1.0.1',
     description='Simple Djagno galleries.',
     long_description=README,
 
