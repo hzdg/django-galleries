5610006b23c588f0519e12101318ad8c92f89e4e
Chris McKenzie <chrismc@hzdg.com>
2014-09-19 16:17:55 -0400

    1.1.1 Release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = read('README.markdown')
 
 setup(
     name='django-galleries',
-    version='1.1.0',
+    version='1.1.1',
     description='Simple Django galleries.',
     long_description=README,
     author='Matthew Tretter',
