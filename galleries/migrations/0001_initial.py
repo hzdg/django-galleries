2caecd54dc6df170c2ca24f35f81d6827badd5c8
Mohammad Taleb <simul14@gmail.com>
2015-01-29 13:01:30 -0500

    Add Django 1.7 migration

--- galleries/migrations/0001_initial.py
+++ galleries/migrations/0001_initial.py
@@ -1,53 +1,37 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'ImageModel'
-        db.create_table(u'galleries_imagemodel', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('original_image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
-        ))
-        db.send_create_signal(u'galleries', ['ImageModel'])
+    dependencies = [
+    ]
 
-        # Adding model 'EmbedModel'
-        db.create_table(u'galleries_embedmodel', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('embed_code', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal(u'galleries', ['EmbedModel'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'ImageModel'
-        db.delete_table(u'galleries_imagemodel')
-
-        # Deleting model 'EmbedModel'
-        db.delete_table(u'galleries_embedmodel')
-
-
-    models = {
-        u'galleries.embedmodel': {
-            'Meta': {'object_name': 'EmbedModel'},
-            'embed_code': ('django.db.models.fields.TextField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'galleries.imagemodel': {
-            'Meta': {'ordering': "['title']", 'object_name': 'ImageModel'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'original_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        }
-    }
-
-    complete_apps = ['galleries']
+    operations = [
+        migrations.CreateModel(
+            name='EmbedModel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50)),
+                ('embed_code', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ImageModel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50)),
+                ('description', models.CharField(max_length=255, blank=True)),
+                ('original_image', models.ImageField(max_length=255, upload_to=b'galleries')),
+            ],
+            options={
+                'ordering': ['title'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
